156e8f3ee6a5b15a561bc246f57248a2ec2576c1
Carl <carlelli5@hotmail.co.uk>
2023-06-07 11:52:25 +0000

    added a post method to holiday list view

--- holiday/views.py
+++ holiday/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Holiday
@@ -6,9 +6,23 @@ from .serializers import HolidaySerializer
 
 
 class HolidayList(APIView):
+    serializer_class = HolidaySerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         holidays = Holiday.objects.all()
         serializer = HolidaySerializer(
             holidays, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = HolidaySerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
