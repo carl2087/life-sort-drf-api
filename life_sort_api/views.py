4d0ff7f7c4945c1d712f0944bd6ea911b0596d12
Carl <carlelli5@hotmail.co.uk>
2023-06-08 15:21:32 +0000

    added welcome message to api and updated pagination also set the api to return json in development

---
+++ life_sort_api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE,
+    JWT_AUTH_SECURE,
+)
+
+
+"""
+Welcome message displayed to users when visiting the API.
+"""
+
+
+@api_view()
+def root_route(request):
+    return Response({
+        'message': "Welcome to the Life sort API!"
+    })
