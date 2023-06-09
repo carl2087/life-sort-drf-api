c820ee575c5408df20bb0f5389eee14bb8f7284b
Carl <carlelli5@hotmail.co.uk>
2023-06-09 15:17:42 +0000

    wrote unit tests for custom task detail view

--- custom_task/tests.py
+++ custom_task/tests.py
@@ -66,3 +66,90 @@ class CustomTaskListViewTests(APITestCase):
                              'due_date': '2023-07-01 12:00', 'start_date':
                              '2023-07-01 12:00', 'budget': '-10'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTaskDetailViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='carltest', password='testcarl')
+        User.objects.create_user(username='junotest', password='testjuno')
+        carltest = User.objects.get(username='carltest')
+        CustomTask.objects.create(
+            owner=carltest, title='a title',
+            due_date='2023-07-01T13:20:30+03:00',
+            start_date='2023-07-01T13:20:30+03:00'
+        )
+
+    def test_logged_in_user_can_list_custom_task(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.get('/customtask/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_invalid_id(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.get('customtask/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_out_user_cannnot_list_custom_task(self):
+        response = self.client.get('/customtask/1/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_view_other_user_custom_task(self):
+        self.client.login(username='junotest', password='testjuno')
+        response = self.client.get('/customtask/1/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_custom_task_owner_can_edit_own_task(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.put(
+            '/customtask/1/', {'title': 'a new title',
+                               'due_date': '2023-07-01 12:00',
+                               'start_date':
+                               '2023-07-01 12:00'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_custom_task_owner_cannot_edit_other_user_task(self):
+        self.client.login(username='junotest', password='testjuno')
+        response_one = self.client.post(
+            '/customtask/', {'title': 'a new title',
+                             'due_date': '2023-07-01 12:00',
+                             'start_date':
+                             '2023-07-01 12:00'})
+        response_two = self.client.put(
+            '/customtask/1/', {'title': 'a new title',
+                               'due_date': '2023-07-01 12:00',
+                               'start_date':
+                               '2023-07-01 12:00'})
+        self.assertEqual(response_one.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_two.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_custom_task_edit_has_valid_dates(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.put(
+            '/customtask/1/', {'title': 'a title',
+                               'due_date': '2023-01-01 12:00', 'start_date':
+                               '2023-01-01 12:00'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_custom_task_edit_has_valid_max_date(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.put(
+            '/customtask/1/', {'title': 'a title',
+                               'due_date': '2050-01-01 12:00', 'start_date':
+                               '2050-01-01 12:00'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_custom_task_edit_has_valid_title(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.put(
+            '/customtask/1/', {'title': '',
+                               'due_date': '2050-01-01 12:00', 'start_date':
+                               '2050-01-01 12:00'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_cannot_edit_minus_budget(self):
+        self.client.login(username='carltest', password='testcarl')
+        response = self.client.put(
+            '/customtask/1/', {'title': 'a title',
+                               'due_date': '2023-07-01 12:00', 'start_date':
+                               '2023-07-01 12:00', 'budget': '-10'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
